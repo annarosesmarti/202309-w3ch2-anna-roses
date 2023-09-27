118345212f9b32c2b787cf48f7520ce78279f2dc
anna-roses <anna.roses.marti@gmail.com>
2023-09-27 15:11:58 +0200

    Add deckGenerator function

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const cardSuits = ["♠️", "♣️", "♥️", "♦️"];
+const cardNumbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+
+const deckGenerator = () => {
+  const cardsDeck = [];
+  cardSuits.forEach((suits) => {
+    cardNumbers.forEach((numbers, position) => {
+      cardsDeck.push({ suits: suits, numbers: numbers, value: position });
+    });
+  });
+  return cardsDeck;
+};
