dae639e117cea7e0b586bf18f9b629a27b0b47e5
anna-roses <anna.roses.marti@gmail.com>
2023-09-28 21:16:33 +0200

    Add functions to check the results of the cards

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
 
 const generateDeck = () => {
   const deckOfCards = [];
+
   cardSuits.forEach((cardSuit) => {
     cardValues.forEach((cardValue, cardValuePosition) => {
       deckOfCards.push({
@@ -12,67 +13,26 @@ const generateDeck = () => {
       });
     });
   });
+
   return deckOfCards;
 };
 
 const getRandomCard = () =>
   generateDeck()[Math.floor(Math.random() * generateDeck().length)];
 
-const startButton = document.querySelector(".button-start");
-const greaterButton = document.querySelector(".button-greater");
-const smallerButton = document.querySelector(".button-smaller");
-
-const cardsDisplay = document.querySelector(".cards-container");
-const suitCardGameTop = document.querySelector(".card-game-suit-top");
-const suitCardGameBottom = document.querySelector(".card-game-suit-bottom");
-const valueCardGame = document.querySelector(".card-game-value");
-
-let visibleRandomCardGame;
-let hiddenRandomCardPlayer;
-
-const clickStartButton = () => {
-  startButton.addEventListener("click", (event) => {
-    visibleRandomCardGame = getRandomCard();
-    hiddenRandomCardPlayer = getRandomCard();
-
-    startButton.classList.add("hidden");
-    cardsDisplay.classList.remove("hidden");
-    greaterButton.classList.remove("hidden");
-    smallerButton.classList.remove("hidden");
-
-    suitCardGameTop.textContent = `${visibleRandomCardGame.suit}`;
-    suitCardGameBottom.textContent = `${visibleRandomCardGame.suit}`;
-    valueCardGame.textContent = `${visibleRandomCardGame.value}`;
-  });
+const compareCards = () => {
+  if (visibleRandomCardGame.score < hiddenRandomCardPlayer.score) {
+    return "greater";
+  } else if (visibleRandomCardGame.score > hiddenRandomCardPlayer.score) {
+    return "smaller";
+  }
 };
 
-clickStartButton();
-
-const suitCardPlayerTop = document.querySelector(".card-player-suit-top");
-const suitCardPlayerBottom = document.querySelector(".card-player-suit-bottom");
-const valueCardPlayer = document.querySelector(".card-player-value");
-const questionCardPlayer = document.querySelector(".card-player-question-mark");
-const displayText = document.querySelector(".display-text");
-
-const clickGreaterButton = () => {
-  greaterButton.addEventListener("click", (event) => {
-    clickStartButton();
-
-    questionCardPlayer.classList.add("hidden");
-    suitCardPlayerTop.classList.remove("hidden");
-    suitCardPlayerBottom.classList.remove("hidden");
-    valueCardPlayer.classList.remove("hidden");
-    suitCardPlayerTop.textContent = `${hiddenRandomCardPlayer.suit}`;
-    suitCardPlayerBottom.textContent = `${hiddenRandomCardPlayer.suit}`;
-    valueCardPlayer.textContent = `${hiddenRandomCardPlayer.value}`;
-    displayText.classList.remove("hidden");
-
-    if (visibleRandomCardGame.score < hiddenRandomCardPlayer.score) {
-      displayText.textContent = "Greater!! 🥳 🎉";
-    } else {
-      displayText.textContent = "Smaller!! 😔";
-    }
-  });
+const checkCardsResult = (choice) => {
+  if (compareCards() === choice) {
+    displayText.textContent = "Congratulations, your guess was right!! 🥳 🎉";
+  } else {
+    displayText.textContent = "oooh, try again!! 😔";
+  }
+  displayText.classList.remove("hidden");
 };
-
-clickGreaterButton();
